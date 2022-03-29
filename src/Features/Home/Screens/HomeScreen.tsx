14b0fd6c904090a6436bc22f0e1e79d5a296709b
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-03-29 20:11:15 -0300

    feat: create home screen

---
+++ src/Features/Home/Screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { StyledContainer, StyledText } from './HomeScreen.styles';
+
+const HomeScreen = () => {
+    return (
+        <StyledContainer>
+            <StyledText>HomeScreen</StyledText>
+        </StyledContainer>
+    );
+}
+
+export default HomeScreen;
