3559cd3b389d9917f2b3c89b962b7ea40bc22979
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-03-29 20:09:22 -0300

    feat: create app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
 
-export default function App() {
+import Routes from './src/routes';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} /> */}
+      <StatusBar barStyle={'light-content'} />
+      <Routes />
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+
+import LoginScreen from '../Features/Login/Screens/LoginScreen';
+import RegisterScreen from '../Features/Register/Screens/RegisterScreen';
+import HomeScreen from '../Features/Home/Screens/HomeScreen';
+
+const Routes = () => {
+    const { Navigator, Screen } = createNativeStackNavigator();
+
+    return (
+        <Navigator screenOptions={{ headerShown: false }}>
+            <Screen name="LoginScreen" component={LoginScreen} />
+            <Screen name="RegisterScreen" component={RegisterScreen} />
+            <Screen name="HomeScreen" component={HomeScreen} />
+        </Navigator>
+    );
+};
+
+export default Routes;
