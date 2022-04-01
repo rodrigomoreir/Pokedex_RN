736b20da804757f39adc99f136780f9e2924dc2f
Rodrigo Moreira <rodrigoalvesm06@gmail.com>
2022-03-31 22:36:28 -0300

    feat: homescreen finalized

--- src/Features/Home/Screens/HomeScreen.styles.ts
+++ src/Features/Home/Screens/HomeScreen.styles.ts
@@ -1,10 +1,53 @@
 import styled from 'styled-components/native';
 
-export const StyledContainer = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
+export const StyledImageBackground = styled.ImageBackground`
+    width: 100%; 
+    height: 100%;
 `
 
-export const StyledText = styled.Text`
+export const StyledSafeAreaView = styled.SafeAreaView`
+    flex: 1;
+`
+
+export const StyledTextContainer = styled.View`
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledTitleContent = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+
+export const StyledTitle = styled.Text`
+    font-size: 40px;
+    font-weight: 600;
+    color: #E3E3E3;
+`
+
+export const StyledLogoImg = styled.Image`
+    width: 44px;
+    height: 44px;
+    margin-left: 13px;
+`
+
+export const StyledSubtitle = styled.Text`
+    margin-top: 10px;
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 15px;
+    color: #E3E3E3;
+`
+
+export const StyledSearchBarContainer = styled.View`
+    padding-horizontal: 75;
+    justify-content: center;
+    align-items: center;
+    margin-top: 30px;
+`
+
+export const StyledScrollView = styled.ScrollView`
+    padding: 11px;
+    padding-top: 77px;
 `
--- src/Features/Home/Screens/HomeScreen.tsx
+++ src/Features/Home/Screens/HomeScreen.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 
-import { StyledContainer, StyledText } from './HomeScreen.styles';
+import {
+    StyledSafeAreaView,
+    StyledImageBackground,
+    StyledTextContainer,
+    StyledTitleContent,
+    StyledTitle,
+    StyledLogoImg,
+    StyledSubtitle,
+    StyledSearchBarContainer,
+    StyledScrollView
+} from './HomeScreen.styles'
 
-const HomeScreen = () => {
+import backgroundHome from '../../../assets/images/backgroundHome.png'
+import backgroundBlur from '../../../assets/images/backgroundBlur.png'
+import logoApp from '../../../assets/icons/logoApp.png'
+
+import Header from '../../../components/Header';
+import SearchBar from '../../../components/SearchBar';
+import CardList from '../../../components/CardList';
+import { ScrollView } from 'react-native';
+
+const LoginScreen = () => {
+    const { navigate } = useNavigation()
+
     return (
-        <StyledContainer>
-            <StyledText>HomeScreen</StyledText>
-        </StyledContainer>
+        <StyledImageBackground source={backgroundHome}>
+            <StyledImageBackground source={backgroundBlur}>
+                <StyledSafeAreaView>
+                    <Header goProfile={() => { }} />
+                    <StyledTextContainer>
+                        <StyledTitleContent>
+                            <StyledTitle>BROWSE</StyledTitle>
+                            <StyledLogoImg source={logoApp} />
+                        </StyledTitleContent>
+                        <StyledSubtitle>Monitore e veja como estão {'\n'}seus Pokémon.</StyledSubtitle>
+                    </StyledTextContainer>
+                    <StyledSearchBarContainer>
+                        <SearchBar placeholder='pesquise...' value='' onChangeText={() => { }} />
+                    </StyledSearchBarContainer>
+                    <StyledScrollView>
+                        <CardList />
+
+                    </StyledScrollView>
+                </StyledSafeAreaView>
+            </StyledImageBackground>
+        </StyledImageBackground>
     );
 }
 
-export default HomeScreen;
+export default LoginScreen;
